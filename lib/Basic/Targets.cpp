c6fdb09fb9bc5cf5c4b52b2d6f39e6349e942f3b
Dylan McKay <dylanmckay34@gmail.com>
2016-09-29 02:29:48 +1300

    [AVR] Update the data layout
    We had previously updated the data layout in AVR-LLVM commit
    5f12f20bafa50b178395177cb98aef7de5f68e73.

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -7158,7 +7158,7 @@ public:
     PtrDiffType = SignedInt;
     SigAtomicType = SignedLong;
     DoubleFormat = &llvm::APFloat::IEEEsingle;
-    resetDataLayout("e-p:16:8:8-i8:8:8-i16:8:8-i32:8:8-i64:8:8-f32:8:8-f64:8:8-n8");
+    resetDataLayout("e-p:16:16:16-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-n8");
   }
 
   void getTargetDefines(const LangOptions &Opts,
